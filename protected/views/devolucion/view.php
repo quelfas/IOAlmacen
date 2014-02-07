2961fd94ba277b125ba1ea6b9773773abd8c4733
quelfas <cristalmedia@gmail.com>
2014-02-07 10:10:30 -0430

    Traduccion del index
    Traduccion del BreadCrumbs
    Traduccion del Menu de Opciones para administrar devolucion(es)

--- protected/views/devolucion/view.php
+++ protected/views/devolucion/view.php
@@ -3,20 +3,20 @@
 /* @var $model Devolucion */
 
 $this->breadcrumbs=array(
-	'Devolucions'=>array('index'),
+	'Devolucion'=>array('index'),
 	$model->id,
 );
 
 $this->menu=array(
-	array('label'=>'List Devolucion', 'url'=>array('index')),
-	array('label'=>'Create Devolucion', 'url'=>array('create')),
-	array('label'=>'Update Devolucion', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Devolucion', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Devolucion', 'url'=>array('admin')),
+	array('label'=>'Lista de Devoluciones', 'url'=>array('index')),
+	array('label'=>'Crear Devolucion', 'url'=>array('create')),
+	array('label'=>'Actualizar Devolucion', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Eliminar Devolucion', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Esta seguro que desea eliminar esta Devolucion?')),
+	array('label'=>'Administrar Devoluciones', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View Devolucion #<?php echo $model->id; ?></h1>
+<h1>Devolucion #<?php echo $model->id; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
