02a28f1a01e3fbe4a45f37c53472435f52277922
quelfas <cristalmedia@gmail.com>
2014-02-07 11:08:05 -0430

    traduccion de update

--- protected/views/devolucion/update.php
+++ protected/views/devolucion/update.php
@@ -3,19 +3,19 @@
 /* @var $model Devolucion */
 
 $this->breadcrumbs=array(
-	'Devolucions'=>array('index'),
+	'Devolucion'=>array('index'),
 	$model->id=>array('view','id'=>$model->id),
-	'Update',
+	'Actualizar',
 );
 
 $this->menu=array(
-	array('label'=>'List Devolucion', 'url'=>array('index')),
-	array('label'=>'Create Devolucion', 'url'=>array('create')),
-	array('label'=>'View Devolucion', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Devolucion', 'url'=>array('admin')),
+	array('label'=>'Lista de Devoluciones', 'url'=>array('index')),
+	array('label'=>'Crear Devolucion', 'url'=>array('create')),
+	array('label'=>'Ver Devolucion', 'url'=>array('view', 'id'=>$model->id)),
+	array('label'=>'Administrar Devoluciones', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Update Devolucion <?php echo $model->id; ?></h1>
+<h1>Actualizacion de Devolucion <?php echo $model->id; ?></h1>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
